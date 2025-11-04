ffb934a0c6c4312ab5938f8a458fcb0c894263d5
jieen <34214763+jieen1@users.noreply.github.com>
2025-11-04 17:31:59 +0800

    修复颜色问题

--- packages/datasheet/src/pc/utils/conditional_format.ts
+++ packages/datasheet/src/pc/utils/conditional_format.ts
@@ -1,7 +1,10 @@
 import { Selectors, IReduxState, ViewFilterDerivate } from '@apitable/core';
+import { setColor } from 'pc/components/multi_grid/format';
+import { useAppSelector } from 'pc/store/react-redux';
 
 export function getConditionalFillColor(state: IReduxState, recordId: string, fieldId?: string): string | undefined {
-  const rules = (Selectors as any).getConditionalFormatRules(state) as any[];
+  const cacheTheme = useAppSelector(Selectors.getTheme);
+  const rules = Selectors.getConditionalFormatRules(state);
   if (!rules || !rules.length) return undefined;
   const datasheetId = Selectors.getActiveDatasheetId(state)!;
   const snapshot = Selectors.getSnapshot(state, datasheetId)!;
@@ -12,7 +15,7 @@ export function getConditionalFillColor(state: IReduxState, recordId: string, fi
     }
     const rows = derivate.getFilterRowsBase({ filterInfo: rule.filterInfo, rows: [{ recordId } as any], recordMap: snapshot.recordMap });
     if (rows && rows.length) {
-      return rule.color as string;
+      return setColor(rule.color, cacheTheme);
     }
   }
   return undefined;
